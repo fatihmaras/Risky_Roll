7d99a2a2a5066a8f71770e34c79216254cde1d37
fatihmaras <103513022+fatihmaras@users.noreply.github.com>
2023-05-02 02:07:05 +0300

    added player movement

--- Risky_Roll/Assets/Scripts/PlayerMovement.cs
+++ Risky_Roll/Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,10 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody _rb;
     private float _horizontalInput, _verticalInput;
-    [SerializeField] private float _speed = 1f; 
+    [SerializeField] private float _speed = 10f; 
+    [SerializeField] private float _jumpForce=10;
+    private bool _isJumpButtonPressed;
+    private bool _isGrounded;
 
 
     // Start is called before the first frame update
@@ -20,6 +23,14 @@ public class PlayerMovement : MonoBehaviour
     {
         _horizontalInput= Input.GetAxis("Horizontal");
         _verticalInput = Input.GetAxis("Vertical");
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            
+            _isJumpButtonPressed=true;
+
+        }
     }
 
     private void FixedUpdate() 
@@ -27,5 +38,32 @@ public class PlayerMovement : MonoBehaviour
         Vector3 playerMovement = new Vector3(_horizontalInput , 0 , _verticalInput);
         playerMovement *= _speed;
         _rb.AddForce(playerMovement,ForceMode.Acceleration);
+
+        Ray ray =new Ray(transform.position,Vector3.down);
+        if(Physics.Raycast(ray, transform.localScale.x/2f+0.01f))
+        {
+            _isGrounded=true;
+        }
+        else
+        {
+            _isGrounded=false;
+        }
+
+
+
+        if(_isJumpButtonPressed && _isGrounded)
+        {
+            _rb.AddForce(Vector3.up* _jumpForce, ForceMode.Impulse);
+            _isJumpButtonPressed = false;
+        }
     }
+
+    // private void OnCollisionEnter(Collision other) 
+    // {
+    //     _isGrounded=true;
+    // }
+    // private void OnCollisionExit(Collision other) 
+    // {
+    //     _isGrounded=false;
+    // }
 }
